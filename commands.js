1c87c8fb9e13604953768d424f2ebdd1085e9537
Ben <budemaso@gmail.com>
2024-03-16 11:55:38 -0600

    Got API hooked up

---
+++ commands.js
@@ -0,0 +1,16 @@
+async function fetchData() {
+    const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities/Q100/nearbyCities?radius=100';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '9ad33a5f7emsh5bf504064de98a9p16e3e4jsn6d60b88b5d1f',
+            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        }
+    };
+
+    const response = await fetch(url, options);
+    const result = await response.json()
+
+    document.getElementById("neighborhoods").innerHTML = result.data.map(item => item.name)
+}
+fetchData();
